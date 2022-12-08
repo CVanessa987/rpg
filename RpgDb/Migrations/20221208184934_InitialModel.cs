044869cf93828119ab11d55afb30134e69edf439
Vanessa <vanessachenon15@gmail.com>
2022-12-08 20:55:22 +0100

    Database creation

---
+++ RpgDb/Migrations/20221208184934_InitialModel.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RpgDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tag = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Potions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Effect = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Potions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuestGoal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Reward = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weapons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Damage = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weapons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Potions");
+
+            migrationBuilder.DropTable(
+                name: "Quests");
+
+            migrationBuilder.DropTable(
+                name: "Weapons");
+        }
+    }
+}
---
+++ RpgDb/Model.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgDb
+{
+    public class Model
+    {
+        public class RpgContext : DbContext
+        {
+            public DbSet<Item> Items { get; set; }
+            public DbSet<QuestItem> Quests { get; set; }
+            public DbSet<WeaponItem> Weapons { get; set; }
+            public DbSet<PotionItem> Potions { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+                    => optionsBuilder.UseSqlServer($"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RpgItemDatabase");
+
+        }
+
+
+        public class Item
+        {
+            public int Id { get; set; }
+            public string Category { get; set; }
+            public string Tag { get; set; }
+        }
+
+        public class PotionItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public string Effect { get; set; }
+
+            public int ItemId { get; set; }
+        }
+
+        public class WeaponItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Damage { get; set; }
+
+            public int ItemId { get; set; }
+        }
+
+        public class QuestItem
+        {
+            public int Id { get; set; }
+            public string Type { get; set; }
+            public string QuestGoal { get; set; }
+            public string Reward { get; set; }
+
+            public int ItemId { get; set; }
+        }
+    }
+}
